30937ca15b6a4da77f83d02025a4d4c260e15002
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-09-09 18:27:29 +0530

    header component init

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <div>
+            <p>This is the header components</p>
+        </div>
+    );
+}
+
+export default Header;
