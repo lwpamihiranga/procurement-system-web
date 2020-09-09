a2c4f6d9ee035c516e5f43637a28ccbd9d19256a
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-09-09 18:26:56 +0530

    login form component init

---
+++ src/components/login-form/LoginForm.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import './LoginForm.css';
+
+function LoginForm() {
+    return (
+        <div>
+            <p>This is the login form components</p>
+        </div>
+    );
+}
+
+export default LoginForm;
