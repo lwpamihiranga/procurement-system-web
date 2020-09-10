97bfc8b04936e74be6c615bea24b40c9e485b022
HusmithaSilva <husmitha.silva0@gmail.com>
2020-09-10 18:30:13 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import LoginPage from './pages/LoginPage';
-import ManagerForm from './components/site-manager/ManagerAddForm'
 
 function App() {
     return (
@@ -12,10 +11,7 @@ function App() {
                     <LoginPage />
                     
                 </Route>
-                <Route exact path="/adduser">
-                    <ManagerForm />
-
-                </Route>
+               
             </Switch>
         </Router>
     );
