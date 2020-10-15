7c37e709becde4a83f98e048ba7271cc12074383
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-10-15 16:54:23 +0530

    adding new routes to show order single view pages

--- src/pages/RequisitionsPage.js
+++ src/pages/RequisitionsPage.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import SideMenu from '../components/side-menu/SideMenu';
 import { Switch, Route } from 'react-router-dom';
-import Requisitions from '../components/requisitions/Requisitions'
-import Header from '../components/header/Header'
-
+import Requisitions from '../components/requisitions/Requisitions';
+import Header from '../components/header/Header';
+import ApproveRequisition from '../components/requisitions/ApproveRequisition';
 
 function DashboardPage() {
     return (
         <Switch>
             <Route exact path="/requisitions">
-                <Header/>
-                <div style={{marginTop:45}}>
+                <Header />
+                <div style={{ marginTop: 45 }}>
                     <SideMenu navName="requisitions" />
                     <Requisitions />
                 </div>
-            </Route>                  
+            </Route>
+            <Route exact path="/requisitions/:id">
+                <Header />
+                <div style={{ marginTop: 45 }}>
+                    <SideMenu navName="requisitions" />
+                    <ApproveRequisition />
+                </div>
+            </Route>
         </Switch>
     );
 }
